fd9a3b2c5748ac23815825d76e95185bcbcf9f8f
Larisa Karelina <karelina.rss@gmail.com>
2020-05-09 13:30:20 +0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        за каждые 20 рублей, потраченные на билет, начисляется 1 миля
+        int price = 17465;
+        int miles = 20;
+        int bonus = price / miles;
+        System.out.println(bonus);
+    }
+}
